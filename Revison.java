258c00345432d56c5317f07fa7a7148a6f2cf6b6
Adnan Khan <beingadnankhan678@gmail.com>
2024-03-29 12:02:13 +0530

    File updated of StacK

--- Revison.java
+++ Revison.java
@@ -1,4 +1,21 @@
-public class Revison {
+import java.util.Stack;
 
-    
+public class Revison {
+    public static void main(String[] args) {
+        Stack<Integer>stc=new Stack<>();
+
+        stc.push(1);
+        stc.push(2);
+        stc.push(3);
+
+       System.out.println( stc.peek());  // 3
+
+       System.out.println(stc.isEmpty());
+
+       System.out.println(stc.pop());
+
+
+
+        System.out.println(stc);
+    }
 }
