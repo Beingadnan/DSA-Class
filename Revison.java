8bc0b904e66e4cb8f58a768dc390c799d9df4ec4
Adnan Khan <beingadnankhan678@gmail.com>
2024-03-29 11:47:42 +0530

    Stack revision

--- Revison.java
+++ Revison.java
@@ -1,19 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Revison {
 
-    public static void main(String[]args){
-        Queue <Integer>queue=new LinkedList<>();
-
-        queue.add(1);
-        queue.offer(2);
-
-        System.out.println(queue.peek());
-        System.out.println(queue.isEmpty());
-        // queue.remove(2);
-        queue.poll();
-        // queue.clear();
-        System.out.println(queue);
-    }
+    
 }
