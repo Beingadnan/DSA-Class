3f8d1206fdf42a419e941d2f470720c5d8425c67
Adnan Khan <beingadnankhan678@gmail.com>
2024-03-29 19:51:00 +0530

    f

--- Recursion.java
+++ Recursion.java
@@ -1,46 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * Recursion
+ */
 public class Recursion {
-    public static void Incresing(int n){
-        if(n==1){
-            System.out.println(1);
-            return;
-        }
-        Incresing(n-1);
-        System.out.println(n);
 
-    }
-   
-    public static void Decreasing(int n){
-        if(n==1){
-            System.out.println(1);
-            return;
-        }
-        System.out.println(n);
-        Decreasing(n-1);
-    }
-
-    public static int Factorial(int n){
-        if(n==0){
-            return 1;
-        }
-        int small=Factorial(n-1);
-        int big=n*small;
-        return big;
-        
-    }
-
-    public static int Fibonacci(int n){
-         if(n==0 || n==1){
-            return n;
-         }
-         int a=Fibonacci(n-1);
-         int b=Fibonacci(n-2);
-         return a+b;
-    }
     public static void main(String[] args) {
-        int n=5;
-        Incresing(n);
-        Decreasing(n);
-        System.out.println(Factorial(n));
-        System.out.println(Fibonacci(2));
+        int arr[] = { 4, 5, 6, 1, 2, 3 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
